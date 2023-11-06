764772338b7ee49f10659acd1a747ec18ece106d
jmacaraeg <jmacaraeg@academic.rrc.ca>
2023-11-05 18:49:08 -0600

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'customers/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :customers, only: [:index] do
+    collection do
+      get 'missing_email'
+      get 'alphabetized'
+    end
+  end
+
+  root 'customers#index'
 end
